128e96d86c63c631c752f3b480a522157dcc9338
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-14 14:45:58 +0200

    add data

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
+	var_dump($json);
 	$text = $json->result->parameters->text;
+	var_dump($text);
 
 	switch ($text) {
 		case 'Salut':
