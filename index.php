f5f347e9ebfa657705a21da2f7369b24992b2b80
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-14 15:44:52 +0200

    add api geo city

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if($method == 'POST'){
 
 	$requestCity = file_get_contents("https://geo.api.gouv.fr/communes?nom=" . $text . "&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre");
 	$jsonCity = json_decode($requestCity);
-
+	var_dump($jsonCity);
 	if ($jsonCity->nom != null) {
 		$speech = $jsonCity->code;
 	} else {
