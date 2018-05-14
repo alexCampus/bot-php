4ff78aa6c0735f3aac67c6ba7e86e1708a174753
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-14 16:34:39 +0200

    add api geo city

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if($method == 'POST'){
 	$resultCity = get_object_vars($jsonCity[0]);
 
 	if ($resultCity['nom'] != null) {
-		$speech = $resultCity['codeDepartement'];
+		$speech = "Le code du département est : " . $resultCity['codeDepartement'] . ' .<br> Et il y a ' .  . number_format($resultCity['population']) . ' habitants.' ;
 	} else {
 		$speech = "Désolé je ne connais pas cette ville.";
 	};
