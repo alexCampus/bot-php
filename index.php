b642384491503efd2aeb81b1be991a28b1ae8428
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-15 10:32:31 +0200

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	
-	$text      = $json->queryResult;
+	$text      = $json->queryResult->parameters;
 	var_dump($text);
 	$cityArray = get_object_vars($text);
 	
