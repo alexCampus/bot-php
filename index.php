52fc2d4c1af9293e7783812d49be90eb27cc7b57
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-15 13:39:40 +0200

    test

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if($method == 'POST'){
 	// foreach ($jsonCity as $key => $value) {
 	// 	array_push($resultCity, $value);
 	// }
+	var_dump($jsonCity);
 	var_dump(count($jsonCity));
 	// $resultCity = get_object_vars($jsonCity[0]);
 
