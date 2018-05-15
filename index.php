656950e0dac6d8350d1a78bef76ed547e26c074f
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-15 15:42:32 +0200

    test

--- index.php
+++ index.php
@@ -44,18 +44,16 @@ if($method == 'POST'){
 		$response->fulfillmentMessages[]['text']['text'] = [$speech];
 	} else {
 		$i = 0;
+		$response->fulfillmentMessages[]['text'] = array(
+			'text' : array()
+		);
 		foreach ($speech as $key => $value) {
 			// $response->fulfillmentText['messages'] = [$speech[$i]->nom . ' est dans le département ' . $speech[$i]->codeDepartement . ' et il y a ' . number_format($speech[$i]->population) . ' habitants.'];
-			$t =  array('text'=>[$speech[$i]->nom . ' est dans le département ' . $speech[$i]->codeDepartement . ' et il y a ' . number_format($speech[$i]->population) . ' habitants.']);
-			
-			// $response->fulfillmentMessages[]['text'] += array(
-			// 	'text' => [$speech[$i]->nom . ' est dans le département ' . $speech[$i]->codeDepartement . ' et il y a ' . number_format($speech[$i]->population) . ' habitants.']
-			// );
+			$response->fulfillmentMessages[]['text']['text'] = [$speech[$i]->nom . ' est dans le département ' . $speech[$i]->codeDepartement . ' et il y a ' . number_format($speech[$i]->population) . ' habitants.'];
 			$i++;
 		}
-		$response->fulfillmentMessages[]['text'] = $t;
-	var_dump($t);
 	}
+	
 	$response->source = "webhook";
 	echo json_encode($response);
 }
