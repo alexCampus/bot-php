d190e35690d51624ea597ebdb5d97bdd6651527c
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-15 15:06:35 +0200

    test

--- index.php
+++ index.php
@@ -44,9 +44,6 @@ if($method == 'POST'){
 		$response->fulfillmentMessages[]['text']['text'] = [$speech];
 	} else {
 		$i = 0;
-		$response->fulfillmentMessages[]['text'] = array(
-			'text' : array()
-		);
 		foreach ($speech as $key => $value) {
 			// $response->fulfillmentText['messages'] = [$speech[$i]->nom . ' est dans le département ' . $speech[$i]->codeDepartement . ' et il y a ' . number_format($speech[$i]->population) . ' habitants.'];
 			// $response->fulfillmentMessages[]['text']['text'] = [$speech[$i]->nom . ' est dans le département ' . $speech[$i]->codeDepartement . ' et il y a ' . number_format($speech[$i]->population) . ' habitants.'];
