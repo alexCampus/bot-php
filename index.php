b246771b43c859ddb99d2c55dcb97960271955ae
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-17 15:24:56 +0200

    test

--- index.php
+++ index.php
@@ -59,7 +59,6 @@ if($method == 'POST'){
 
 		$response->fulfillmentText[0] = "Super j'adore " . $music . " moi aussi.  \n  \n Mes titres préférés sont  \n  \n : " . $array[0] . " et   \n  \n" . $array[1];
 		$response->fulfillmentMessages[]['text']['text'][0] = "Super j'adore " . $music . " moi aussi.  \n  \n Mes albums préférés sont  \n  \n : " . $array[0] . " et  \n  \n " . $array[1];
-		$response->fulfillmentMessages[]['text']['text'] .= ["Super j'adore " . $music . " moi aussi.  \n  \n Mes albums préférés sont  \n  \n : " . $array[0] . " et  \n  \n " . $array[1]];
 		
 	}
 	
