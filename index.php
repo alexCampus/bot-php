c1e923dc8cafa15dc5cb12d3092d91b922726892
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-17 14:03:01 +0200

    test

--- index.php
+++ index.php
@@ -19,34 +19,40 @@ if($method == 'POST'){
 
 	$text      = $json->queryResult->parameters->ville;
 	$music     = $json->queryResult->parameters->music-artist;
-	$requestCity = file_get_contents("https://geo.api.gouv.fr/communes?nom=" . skip_accents($text) . "&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre");
-	$jsonCity = json_decode($requestCity);
+	if ($text != null) {
+		$requestCity = file_get_contents("https://geo.api.gouv.fr/communes?nom=" . skip_accents($text) . "&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre");
+		$jsonCity = json_decode($requestCity);
 
-	if (count($jsonCity) === 1) {
-		$speech = $text . " est dans le département : " . $jsonCity[0]->codeDepartement . ' . Et il y a ' . number_format($jsonCity[0]->population) . ' habitants.Quel est ton groupe de musique préféré?' ;
-	} elseif (count($jsonCity) > 1) {
-		$i = 0;
-		foreach ($jsonCity as $key => $value) {
-			$speech[$i] = $value;
-			$i++;
-		}
-	} else {
-		$speech = "Désolé je ne connais pas cette ville.";
-	};
-
-	$response = new \stdClass();
-	if (count($speech) === 1) {
-		$response->fulfillmentText = $speech;
-		$response->fulfillmentMessages[]['text']['text'] = [$speech];
-	} else {
-		$i = 0;
-		foreach ($speech as $key => $value) {
-			if ($speech[$i]->nom === $text) {
-				$response->fulfillmentMessages[]['text']['text'] = [$speech[$i]->nom . ' est dans le département ' . $speech[$i]->codeDepartement . ' et il y a ' . number_format($speech[$i]->population) . ' habitants. Quel est ton groupe de musique préféré?' ];
+		if (count($jsonCity) === 1) {
+			$speech = $text . " est dans le département : " . $jsonCity[0]->codeDepartement . ' . Et il y a ' . number_format($jsonCity[0]->population) . ' habitants.Quel est ton groupe de musique préféré?' ;
+		} elseif (count($jsonCity) > 1) {
+			$i = 0;
+			foreach ($jsonCity as $key => $value) {
+				$speech[$i] = $value;
+				$i++;
 			}
-			$i++;
+		} else {
+			$speech = "Désolé je ne connais pas cette ville.";
+		};
+
+		$response = new \stdClass();
+		if (count($speech) === 1) {
+			$response->fulfillmentText = $speech;
+			$response->fulfillmentMessages[]['text']['text'] = [$speech];
+		} else {
+			$i = 0;
+			foreach ($speech as $key => $value) {
+				if ($speech[$i]->nom === $text) {
+					$response->fulfillmentMessages[]['text']['text'] = [$speech[$i]->nom . ' est dans le département ' . $speech[$i]->codeDepartement . ' et il y a ' . number_format($speech[$i]->population) . ' habitants. Quel est ton groupe de musique préféré?' ];
+				}
+				$i++;
+			}
 		}
+	} elseif ($music != null) {
+		$response->fulfillmentText = "Super j'adore cet artiste moi aussi";
+		$response->fulfillmentMessages[]['text']['text'] = ["Super j'adore cet artiste moi aussi"];
 	}
+	
 	
 	$response->source = "webhook";
 	echo json_encode($response);
