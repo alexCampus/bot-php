857858bb3197c3139b13f35ec20f46804faea0b4
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-18 14:45:55 +0200

    test

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if($method == 'POST'){
 		// $response->fulfillmentText[]['text']['text'][0] = "Super j'adore " . $music . " moi aussi.  \n  \n Mes titres préférés sont  \n  \n : " . $array[0] . " et   \n  \n" . $array[1];
 		// $response->fulfillmentText['text']['text'][1] = "Super j'adore " . $music . " moi aussi.  \n  \n Mes titres préférés sont  \n  \n : " . $array[0] . " et   \n  \n" . $array[1];
 		$response->fulfillmentMessages[]['text']['text'][0] = "Super j'adore " . $music . " moi aussi.  \n  \n Mes albums préférés sont  \n  \n : " . $array[0] . " et  \n  \n " . $array[1];
-		$response->fulfillmentMessages[]['text']['text'][0][] = "Super j'adore " . $music . " moi aussi.  \n  \n Mes albums préférés sont  \n  \n : " . $array[0] . " et  \n  \n " . $array[1];
+		$response->fulfillmentMessages[]['text']['text'][0] = "Super j'adore " . $music . " moi aussi.  \n  \n Mes albums préférés sont  \n  \n : " . $array[0] . " et  \n  \n " . $array[1];
 	}
 	
 	
